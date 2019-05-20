21e27ce36eb92c1b23ed7dac4afabf6a3ae43b53
David <junjian.chen@foxmail.com>
2019-05-20 22:04:36 +0800

    完成番茄闹钟列表

--- src/components/Tomatoes/Tomatoes.tsx
+++ src/components/Tomatoes/Tomatoes.tsx
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './Tomatoes.scss'
 import TomatoAction from './TomatoAction'
+import TomatoList from './TomatoList'
 import { addTomato, initTomatoes, updateTomato } from 'redux/actions/tomatoes';
 import axios from '../../config/axios'
+import _ from 'lodash' 
+import { format } from "date-fns";
 
 const mapStateToProps = (state, ownProps) => ({
   tomatoes: state.tomatoes,
@@ -33,6 +36,16 @@ class Tomatoes extends Component<Props> {
     return this.props.tomatoes.filter(t => !t.description && !t.ended_at && !t.aborted)[0]
   }
 
+  get finishedTomatoes() {
+    const finishedTomatoes = this.props.tomatoes.filter(t => t.description && t.ended_at && !t.aborted)
+    const obj = _.groupBy(finishedTomatoes, (tomato)=>{ //lodasha
+      return format(tomato.started_at, 'YYYY-MM-D')
+    })
+    console.log(obj);
+    
+    return obj  //返回一个以日期为keys，当天完成的tomatoes（数组）为values的对象
+  }
+
   getTomatoes = async ()=> {
     try{
       const response = await axios.get('tomatoes')
@@ -60,6 +73,7 @@ class Tomatoes extends Component<Props> {
         unfinishedTomato={this.unfinishedTomato} 
         updateTomato={this.props.updateTomato}
         />
+        <TomatoList finishedTomatoes={this.finishedTomatoes} />
       </div>
     );
   }
